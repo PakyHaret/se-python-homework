47bd91993cd7b2efc990bf50ded8c685749a76b1
Paky <haret_paky@yahoo.com>
2021-10-05 16:13:18 +0300

    Rezolvare finala pentru exercitiu 15 sesiunea 2.

--- session2/ex15.py
+++ session2/ex15.py
@@ -6,3 +6,13 @@
         Veti primi: 'cmi'
         Veti printa: 1
 """
+#Rezolvare:
+x = input()
+i = 0
+j = ["a", "e", "i", "o", "u"]
+for litera in x.lower():
+    for vocala in j:
+        if litera == vocala:
+            i = i + 1
+
+print(i)
