0889c6be86beaaa1f306c13e538328a212fe5013
Paky <haret_paky@yahoo.com>
2021-10-05 16:02:33 +0300

    Rezolvare finala pentru exercitiu 12 sesiunea 2.

--- session2/ex12.py
+++ session2/ex12.py
@@ -8,3 +8,17 @@
         Veti primi 6, veti afisa [1, 2, 3, 4, 5]
         Veti primi 5, veti afisa [1, 4, 9, 16]
 """
+
+#Rezolvare:
+x = int(input())
+i = 1
+j = []
+while x > i:
+    if x % 2 == 0:
+        j.append(i)
+        i = i + 1
+    else:
+        j.append(i * i)
+        i = i + 1
+
+print(j)
