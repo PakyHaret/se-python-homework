b8f45a85b2e84d030c78b1b6dd0ed69013b7392f
Paky <haret_paky@yahoo.com>
2021-10-06 16:16:44 +0300

    Rezolvare finala pentru exercitiu 29 sesiunea 2.

--- session2/ex29.py
+++ session2/ex29.py
@@ -9,3 +9,20 @@
         2 (pentru vocale)
         4 (pentru consoane)
 """
+#Rezolvare:
+x = input()
+i = 0
+j = 0
+lista_vocale = ["a", "e", "i", "o", "u"]
+
+for litera in x:
+    for vocala in lista_vocale:
+        if litera == vocala:
+            i = i + 1
+    if litera != vocala:
+        j = j + 1
+
+j = j - i
+
+print(i)
+print(j)
