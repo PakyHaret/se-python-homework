c054416d960f923a8a2e6337e3dc67baf2de6cd2
Paky <haret_paky@yahoo.com>
2021-10-31 23:26:45 +0200

    Rezolvare finala pentru exercitiu 18 sesiunea 3.

--- session3/ex18.py
+++ session3/ex18.py
@@ -6,3 +6,13 @@
         - f([1,2,3])
             ---> 6
 """
+
+
+def getSum(iterable):
+    if not iterable:
+        return 0
+    else:
+        return iterable[0] + getSum(iterable[1:])
+
+
+print(getSum([1, 5, 7, 12, 15]))
